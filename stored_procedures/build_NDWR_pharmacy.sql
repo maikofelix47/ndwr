747fc4b7d47fce66965603e21b1da0091a640a54
maikofelix47 <maikofelix89@gmail.com>
2020-06-25 11:11:11 +0300

    Added sync to pharmacy procedure

--- stored_procedures/build_NDWR_pharmacy.sql
+++ stored_procedures/build_NDWR_pharmacy.sql
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS ndwr_pharmacy (
                             set @write_table = concat("ndwr_pharmacy_temp_",queue_number);
                             set @queue_table = concat("ndwr_pharmacy_build_queue_",queue_number);                    												
 
-										  SET @dyn_sql=CONCAT('create table if not exists ',@write_table,' like ',@primary_table);
+										        SET @dyn_sql=CONCAT('create table if not exists ',@write_table,' like ',@primary_table);
 							              PREPARE s1 from @dyn_sql; 
 							              EXECUTE s1; 
 							              DEALLOCATE PREPARE s1;  
@@ -57,21 +57,41 @@ CREATE TABLE IF NOT EXISTS ndwr_pharmacy (
 							              EXECUTE s1; 
 							              DEALLOCATE PREPARE s1;  
                                           
-					SET @person_ids_count = 0;
-                    SET @dyn_sql=CONCAT('select count(*) into @person_ids_count from ',@queue_table); 
-                    PREPARE s1 from @dyn_sql; 
-                    EXECUTE s1; 
-                    DEALLOCATE PREPARE s1;
+					                  SET @person_ids_count = 0;
+                            SET @dyn_sql=CONCAT('select count(*) into @person_ids_count from ',@queue_table); 
+                            PREPARE s1 from @dyn_sql; 
+                            EXECUTE s1; 
+                            DEALLOCATE PREPARE s1;
 
-                   SELECT @person_ids_count AS 'num patients to build';
+                            SELECT @person_ids_count AS 'num patients to build';
                    
-                   SET @dyn_sql=CONCAT('delete t1 from ',@primary_table,' t1 join ', @queue_table ,' t2 on (t2.person_id = t1.PatientID);'); 
-				   SELECT CONCAT('Deleting patient records in interim ', @primary_table);
-				   PREPARE s1 from @dyn_sql; 
-				   EXECUTE s1; 
-				   DEALLOCATE PREPARE s1;  
+                            SET @dyn_sql=CONCAT('delete t1 from ',@primary_table,' t1 join ', @queue_table ,' t2 on (t2.person_id = t1.PatientID);'); 
+				                    SELECT CONCAT('Deleting patient records in interim ', @primary_table);
+				                    PREPARE s1 from @dyn_sql; 
+				                    EXECUTE s1; 
+				                    DEALLOCATE PREPARE s1;  
 
-				  end if;
+				              end if;
+                      if (@query_type="sync") then
+                            select 'SYNCING..........................................';
+                            set @write_table = concat("ndwr_pharmacy_temp_",queue_number);
+                            set @queue_table = "ndwr_pharmacy_sync_queue";
+                            CREATE TABLE IF NOT EXISTS ndwr_pharmacy_sync_queue (
+                                person_id INT PRIMARY KEY
+                            );                            
+                            
+                            set @last_update = null;
+                            SELECT 
+                                MAX(date_updated)
+                            INTO @last_update FROM
+                                ndwr.flat_log
+                            WHERE
+                                table_name = @table_version;
+
+                            replace into ndwr_pharmacy_sync_queue
+                             (select distinct person_id from etl.flat_hiv_summary_v15b where date_created >= @last_update);
+
+                      end if;
 
                     set @total_time=0;
                     set @cycle_number = 0;

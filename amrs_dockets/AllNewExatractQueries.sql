cb80516873fec4f9e948158d1cc7ec74f757f071
maikofelix47 <maikofelix89@gmail.com>
2021-05-12 16:27:30 +0300

    Modified enhanced adherence query aliases and added adherence extract query

--- amrs_dockets/AllNewExatractQueries.sql
+++ amrs_dockets/AllNewExatractQueries.sql
@@ -277,59 +277,105 @@ FROM
 -----------------------------------------------------
 
   #6.Enhanced Adherence Counselling
-  SELECT
-  PatientPK,
-  SiteCode,
-  PatientID,
-  Emr,
-  Project,
-  FacilityName,
-  VisitID,
-  VisitDate,
-  SessionNumber,
-  FirstSessionDate,
-  PillCountAdherence,
-  MMAS4_1 as 'MMAS4-1',
-  MMAS4_2 as 'MMAS4-2',
-  MMAS4_3 as 'MMAS4-3',
-  MMAS4_4 as 'MMAS4_4',
-  MMSA8_1,
-  MMSA4_2,
-  MMSA4_3,
-  MMSA4_4,
-  MMSAScore,
-  EACRecievedVL,
-  EACVL,
-  EACVLConcerns,
-  EACVLThoughts,
-  EACWayForward,
-  EACCognitiveBarrier,
-  EACBehaviouralBarrier_1,
-  EACBehaviouralBarrier_2,
-  EACBehaviouralBarrier_3,
-  EACBehaviouralBarrier_4,
-  EACBehaviouralBarrier_5,
-  EACEmotionalBarriers_1,
-  EACEmotionalBarriers_2,
-  EACEconBarrier_1,
-  EACEconBarrier_2,
-  EACEconBarrier_3,
-  EACEconBarrier_4,
-  EACEconBarrier_5,
-  EACEconBarrier_6,
-  EACEconBarrier_7,
-  EACEconBarrier_8,
-  EACReviewImprovement,
-  EACReviewMissedDoses,
-  EACReviewStrategy,
-  EACReferral,
-  EACReferralApp,
-  EACReferralExperience,
-  EACHomevisit,
-  EACAdherencePlan,
-  EACFollowupDate,
-  FROM
-  ndwr.ndwr_patient_eac;
+SELECT 
+    PatientPK,
+    SiteCode,
+    PatientID,
+    Emr,
+    Project,
+    FacilityName,
+    VisitID,
+    VisitDate,
+    SessionNumber,
+    FirstSessionDate,
+    PillCountAdherence,
+    CASE
+     WHEN MMAS4_1 = 1 THEN 'YES'
+     WHEN MMAS4_1 = 0 THEN 'NO'
+     ELSE NULL
+    END AS 'MMAS4-1',
+    CASE
+     WHEN MMAS4_2 = 1 THEN 'YES'
+     WHEN MMAS4_2 = 0 THEN 'NO'
+     ELSE NULL
+    END AS 'MMAS4-2',
+	CASE
+     WHEN MMAS4_3 = 1 THEN 'YES'
+     WHEN MMAS4_3 = 0 THEN 'NO'
+     ELSE NULL
+    END AS 'MMAS4-3',
+    CASE
+     WHEN MMAS4_4 = 1 THEN 'YES'
+     WHEN MMAS4_4 = 0 THEN 'NO'
+     ELSE NULL
+    END AS 'MMAS4_4',
+	CASE
+     WHEN MMAS4_4 = 1 THEN 'YES'
+     WHEN MMAS4_4 = 0 THEN 'NO'
+     ELSE NULL
+    END AS 'MMAS4_4',
+    CASE
+     WHEN MMSA8_1 = 1 THEN 'YES'
+     WHEN MMSA8_1 = 0 THEN 'NO'
+     ELSE NULL
+    END AS 'MMSA8_1',
+    CASE
+     WHEN MMSA4_2 = 1 THEN 'YES'
+     WHEN MMSA4_2 = 0 THEN 'NO'
+     ELSE NULL
+    END AS 'MMSA4_2',
+    CASE
+     WHEN MMSA4_3 = 1 THEN 'YES'
+     WHEN MMSA4_3 = 0 THEN 'NO'
+     ELSE NULL
+    END AS 'MMSA4_3',
+    CASE
+     WHEN MMSA4_4 = 0 THEN 'Never/Rarely'
+     WHEN MMSA4_4 = 0.25 THEN 'Once in a while'
+     WHEN MMSA4_4 = 0.5 THEN 'Sometimes'
+     WHEN MMSA4_4 = 0.75 THEN 'Usually'
+     WHEN MMSA4_4 = 1 THEN 'All the time'
+     ELSE NULL
+    END AS 'MMSA4_4',
+    CASE
+     WHEN MMSAScore >= 0 AND MMSAScore <= 0.25 THEN 'Good'
+     WHEN MMSAScore >= 0.5 AND MMSAScore <= 2 THEN 'Inadequate'
+     WHEN MMSAScore >= 3 AND MMSAScore <= 8 THEN 'Poor'
+     ELSE 'Unknown'
+    END AS 'MMSAScore',
+    EACRecievedVL,
+    EACVL,
+    EACVLConcerns,
+    EACVLThoughts,
+    EACWayForward,
+    EACCognitiveBarrier,
+    EACBehaviouralBarrier_1,
+    EACBehaviouralBarrier_2,
+    EACBehaviouralBarrier_3,
+    EACBehaviouralBarrier_4,
+    EACBehaviouralBarrier_5,
+    EACEmotionalBarriers_1,
+    EACEmotionalBarriers_2,
+    EACEconBarrier_1,
+    EACEconBarrier_2,
+    EACEconBarrier_3,
+    EACEconBarrier_4,
+    EACEconBarrier_5,
+    EACEconBarrier_6,
+    EACEconBarrier_7,
+    EACEconBarrier_8,
+    EACReviewImprovement,
+    EACReviewMissedDoses,
+    EACReviewStrategy,
+    EACReferral,
+    EACReferralApp,
+    EACReferralExperience,
+    EACHomevisit,
+    EACAdherencePlan,
+    EACFollowupDate,
+    DateCreated
+FROM
+    ndwr.ndwr_patient_eac;
 
   --------------------------------------------------------------
 
